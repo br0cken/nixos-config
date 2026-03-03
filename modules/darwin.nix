6e6d4f01129c9af3d22bb442ebba53984a82b121
Tobias Schuett <schuett.tobias@gmail.com>
2026-03-03 09:35:04 +0100

    added config for mas apps

--- modules/darwin.nix
+++ modules/darwin.nix
@@ -33,5 +33,24 @@
       "spotify"
       "zed"
     ];
+    masApps = {
+      "Amphetamine" = 937984704;
+      "Anybox" = 1593408455;
+      "Drafts" = 1435957248;
+      "GarageBand" = 682658836;
+      "Home Assistant" = 1099568401;
+      "iMovie" = 408981434;
+      "Keynote" = 409183694;
+      "Numbers" = 409203825;
+      "Pages" = 409201541;
+      "Parcel" = 375589283;
+      "StopTheMadness Pro" = 6471380298;
+      "Things" = 904280696;
+      "Vidimote" = 1585702412;
+      "WhatsApp" = 310633997;
+      "WireGuard" = 1451685025;
+      "Xcode" = 497799835;
+      "xSearch" = 1579902068;
+    };
   };
 }
